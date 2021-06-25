a133a9dda892469eb7d39a9bed7f5bc5090c08dc
sheeenazien8 <sheenazien08@gmail.com>
2021-06-25 22:28:58 +0700

    rename: test_file

--- tests/BackendResultTest.php
+++ tests/BackendResultTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase as FrameworkTestCase;
  * Class TestCase
  * @author sheeenazien8
  */
-class TestCaseTest extends FrameworkTestCase
+class BackendResultTest extends FrameworkTestCase
 {
     /** @test */
     public function it_can_response_true(): void
